e6321c37e5c5c809af25730951608cb407f59988
ale1105-git <ioana-alexandra.trion@student.upt.ro>
2026-03-31 14:25:37 +0300

    Initial commit with a Hello World JavaFX application

---
+++ src/main/java/com/example/studentmanagera/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.studentmanagera;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
